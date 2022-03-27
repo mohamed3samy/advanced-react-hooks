f5baaa5412d5a36eb4c5f2128f6ab8bced0b7a1f
mohamed3samy <mohamed701samy@gmail.com>
2022-03-27 19:34:48 +0200

    add useCallback example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import UseRefExample3 from "./components/useRef/UseRefExample3";
-import UseMemoExample from "./components/useMemo/UseMemoExample";
+// import UseMemoExample from "./components/useMemo/UseMemoExample";
+import UseCallbackExample from "./components/useCallback/UseCallbackExample";
 
 function App() {
 	return (
 		<div className="container mt-5">
-			<UseMemoExample />
+			<UseCallbackExample />
 		</div>
 	);
 }
